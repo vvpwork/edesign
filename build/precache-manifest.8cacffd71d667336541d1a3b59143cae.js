848b585bfb96e32b78caccc84e7cdcd031b97df1
vvpwork <vovvva33@gmail.com>
2019-11-23 23:00:38 +0200

    heroku_deploy

---
+++ build/precache-manifest.8cacffd71d667336541d1a3b59143cae.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22cf2444cda7dd524e7b62c4b7a7f283",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30b7a4a929523249aeca",
+    "url": "/static/css/1.18585a14.chunk.css"
+  },
+  {
+    "revision": "792d4f99c6c6b7aa20e4",
+    "url": "/static/css/main.d7a80b86.chunk.css"
+  },
+  {
+    "revision": "4b021787553a870a604d",
+    "url": "/static/js/0.77389590.chunk.js"
+  },
+  {
+    "revision": "30b7a4a929523249aeca",
+    "url": "/static/js/1.e16576f7.chunk.js"
+  },
+  {
+    "revision": "8721a51736edefe9d88f",
+    "url": "/static/js/10.5fdcc699.chunk.js"
+  },
+  {
+    "revision": "27c2f7c3bc53be35eda9",
+    "url": "/static/js/11.56be0374.chunk.js"
+  },
+  {
+    "revision": "06d52226d936c16e6430",
+    "url": "/static/js/2.07f7b1b8.chunk.js"
+  },
+  {
+    "revision": "4fdea3d986931d1c8d55",
+    "url": "/static/js/5.76e40a04.chunk.js"
+  },
+  {
+    "revision": "68558547fc2ce665b92d",
+    "url": "/static/js/6.8c893b7f.chunk.js"
+  },
+  {
+    "revision": "4c2d2c41b4efe3feb058",
+    "url": "/static/js/7.c81da935.chunk.js"
+  },
+  {
+    "revision": "db14012748e872dc5c88",
+    "url": "/static/js/8.6c0958e6.chunk.js"
+  },
+  {
+    "revision": "cb66d587464345aeb209",
+    "url": "/static/js/9.0df11d83.chunk.js"
+  },
+  {
+    "revision": "792d4f99c6c6b7aa20e4",
+    "url": "/static/js/main.b3b151ad.chunk.js"
+  },
+  {
+    "revision": "1b73f5b293d83612cc6a",
+    "url": "/static/js/runtime~main.b1d5aaec.js"
+  },
+  {
+    "revision": "ec50ee1633d1684fd0431d772c35cca8",
+    "url": "/static/media/FuturaPT-Heavy.ec50ee16.woff"
+  },
+  {
+    "revision": "92aa0d7878fe4d8d55dab98aae0796e5",
+    "url": "/static/media/FuturaPT-Light.92aa0d78.woff"
+  },
+  {
+    "revision": "995c64e152e8dc077222766889194951",
+    "url": "/static/media/FuturaPT-Medium.995c64e1.woff"
+  },
+  {
+    "revision": "41fcd025702171a274b0d84f94a51b24",
+    "url": "/static/media/Graphic_design.41fcd025.svg"
+  },
+  {
+    "revision": "7c85a8c080667ad171f994dc313fbf40",
+    "url": "/static/media/HowWeWork-1.7c85a8c0.svg"
+  },
+  {
+    "revision": "272e472ca2e48555ba73935206d1b24e",
+    "url": "/static/media/HowWeWork-2.272e472c.svg"
+  },
+  {
+    "revision": "47ef46e7bf37d3cc278ff0997b97d0a2",
+    "url": "/static/media/HowWeWork-3.47ef46e7.svg"
+  },
+  {
+    "revision": "0a07a28d9436bea4c7256751a632194f",
+    "url": "/static/media/HowWeWork-4.0a07a28d.svg"
+  },
+  {
+    "revision": "e6f287db89bfbaaf169bf27a402588e7",
+    "url": "/static/media/MadeEvolveBold.e6f287db.otf"
+  },
+  {
+    "revision": "eb040b1ff623249c1a49f7f1e9621bb2",
+    "url": "/static/media/OpenButton.eb040b1f.svg"
+  },
+  {
+    "revision": "c1e28eff350b1c0a98737029421f6303",
+    "url": "/static/media/about_us_bg.c1e28eff.svg"
+  },
+  {
+    "revision": "0b9ccce55cc44acff03e2b99f267bfd8",
+    "url": "/static/media/about_us_text.0b9ccce5.svg"
+  },
+  {
+    "revision": "14513ed091aab5f1417ddb9fe4a84853",
+    "url": "/static/media/bg_contact_desktop.14513ed0.svg"
+  },
+  {
+    "revision": "3f7d1f47930ddcca9bfecedd6f4ae040",
+    "url": "/static/media/bg_ux_bottom_.3f7d1f47.svg"
+  },
+  {
+    "revision": "50c3b301ba1d60da5308b0190ec77ab2",
+    "url": "/static/media/contact_bg_mobile.50c3b301.svg"
+  },
+  {
+    "revision": "0c0e3c5767bf48b28711694185c6f5cf",
+    "url": "/static/media/instagram-logo.0c0e3c57.svg"
+  },
+  {
+    "revision": "8ac5e5c58a53c5a03a09398be9c31c3c",
+    "url": "/static/media/logo.8ac5e5c5.svg"
+  },
+  {
+    "revision": "f2cc1749feb8909eaafa84d1b02fd968",
+    "url": "/static/media/openButtonViolet.f2cc1749.svg"
+  },
+  {
+    "revision": "3c94f6b10b5a5bde9a7f2211be929a91",
+    "url": "/static/media/project-1.3c94f6b1.png"
+  },
+  {
+    "revision": "bd002deb34e65633f876e7a96c476b7e",
+    "url": "/static/media/project_desktop.bd002deb.png"
+  },
+  {
+    "revision": "c9e5effa9da74cfa705eb582c7f7a4a1",
+    "url": "/static/media/project_page_bg.c9e5effa.png"
+  },
+  {
+    "revision": "ecea5b72287e10b9604cc63c0cf91f4c",
+    "url": "/static/media/rightArrow.ecea5b72.svg"
+  },
+  {
+    "revision": "63781b2f6554ae04ce74076515193e3e",
+    "url": "/static/media/slick-next.63781b2f.svg"
+  },
+  {
+    "revision": "9e18811e6275eaa50bec3967788ab0d6",
+    "url": "/static/media/slick-prev.9e18811e.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "a791cae4b030eb928c8c7b671c727156",
+    "url": "/static/media/telegram-logo.a791cae4.svg"
+  },
+  {
+    "revision": "43c5c033396ba78896d3891626743c89",
+    "url": "/static/media/ux_bg_desktop.43c5c033.svg"
+  },
+  {
+    "revision": "d0771654d2f73c5ea36650a4d5f7f619",
+    "url": "/static/media/viber.d0771654.svg"
+  },
+  {
+    "revision": "175be8f0124371e19f3814d199ca34dc",
+    "url": "/static/media/web_design.175be8f0.svg"
+  }
+]);
